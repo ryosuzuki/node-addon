fb4ae37ba0c9ac33a3f162fc2b1a30d9b10633f5
Ryo Suzuki <ryosuzk@gmail.com>
2016-02-27 01:26:07 -0700

    initial commit

---
+++ build/Release/.deps/Release/obj.target/addon/hello.o.d
@@ -0,0 +1,13 @@
+cmd_Release/obj.target/addon/hello.o := /usr/bin/clang++ '-D_DARWIN_USE_64_BIT_INODE=1' '-D_LARGEFILE_SOURCE' '-D_FILE_OFFSET_BITS=64' '-DBUILDING_NODE_EXTENSION' -I/Users/ryosuzuki/.node-gyp/0.12.0/src -I/Users/ryosuzuki/.node-gyp/0.12.0/deps/uv/include -I/Users/ryosuzuki/.node-gyp/0.12.0/deps/v8/include  -Os -gdwarf-2 -mmacosx-version-min=10.5 -arch x86_64 -Wall -Wendif-labels -W -Wno-unused-parameter -fno-rtti -fno-exceptions -fno-threadsafe-statics -fno-strict-aliasing -MMD -MF ./Release/.deps/Release/obj.target/addon/hello.o.d.raw  -c -o Release/obj.target/addon/hello.o ../hello.cc
+Release/obj.target/addon/hello.o: ../hello.cc \
+  /Users/ryosuzuki/.node-gyp/0.12.0/src/node.h \
+  /Users/ryosuzuki/.node-gyp/0.12.0/deps/v8/include/v8.h \
+  /Users/ryosuzuki/.node-gyp/0.12.0/deps/v8/include/v8stdint.h \
+  /Users/ryosuzuki/.node-gyp/0.12.0/deps/v8/include/v8config.h \
+  /Users/ryosuzuki/.node-gyp/0.12.0/src/node_version.h
+../hello.cc:
+/Users/ryosuzuki/.node-gyp/0.12.0/src/node.h:
+/Users/ryosuzuki/.node-gyp/0.12.0/deps/v8/include/v8.h:
+/Users/ryosuzuki/.node-gyp/0.12.0/deps/v8/include/v8stdint.h:
+/Users/ryosuzuki/.node-gyp/0.12.0/deps/v8/include/v8config.h:
+/Users/ryosuzuki/.node-gyp/0.12.0/src/node_version.h:
